ae44baa4f922e8a636867964e596bee6cd27452b
Henrique Freire <h.freire02@gmail.com>
2019-03-25 19:38:01 -0300

    Inicio tela inicial

--- Tis3Oficina/Tis3Oficina/MainWindow.xaml.cs
+++ Tis3Oficina/Tis3Oficina/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Tis3Oficina
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
