c376627a2a057eabdb1f6ec5fae0f10cd25e6592
Caique <caiiquexd@gmail.com>
2019-04-18 19:12:17 -0300

    Todas restrições de cliente implementadas
    Nesse commit foi feita a validação de todos os campos do cadastro dos clientes, menos o endereço e observações.
    
    Opções para escolher se é pessoa jurídica ou pessoa física.
    
    Adicionado ao projeto o WPF Extended toolkit que permitiu a criação das MaskedTextBox.

--- Tis3Oficina/Tis3Oficina/obj/Debug/Tis3Oficina_Content.g.i.cs
+++ Tis3Oficina/Tis3Oficina/obj/Debug/Tis3Oficina_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("src/assets/icons/user-alt.svg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("carro.ico")]
 
 
---
+++ Tis3Oficina/Tis3Oficina/src/Telas/Alerta.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tis3Oficina.src.Telas
+{
+    /// <summary>
+    /// Lógica interna para Alerta.xaml
+    /// </summary>
+    public partial class Alerta : Window
+    {
+        public Alerta()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
