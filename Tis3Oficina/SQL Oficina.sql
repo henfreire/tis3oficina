7f5f91b1e8212312adffc87972c21a89d092e132
Henrique Freire <h.freire02@gmail.com>
2019-05-14 20:21:51 -0300

    Atualização arquivosql
    Inclusão coluna item orçamento

--- Tis3Oficina/SQL Oficina.sql
+++ Tis3Oficina/SQL Oficina.sql
@@ -1,152 +1,109 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 24-Abr-2019 às 22:09
--- Versão do servidor: 10.1.38-MariaDB
--- versão do PHP: 7.2.16
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `oficina`
---
-
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.1.38-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              10.1.0.5464
 -- --------------------------------------------------------
 
---
--- Estrutura da tabela `cliente`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE `cliente` (
-  `id` int(11) NOT NULL,
+
+-- Copiando estrutura do banco de dados para oficina
+CREATE DATABASE IF NOT EXISTS `oficina` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `oficina`;
+
+-- Copiando estrutura para tabela oficina.cliente
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) DEFAULT NULL,
   `cpf` varchar(14) DEFAULT NULL,
   `telefone` varchar(20) DEFAULT NULL,
   `email` varchar(30) DEFAULT NULL,
   `endereco` varchar(50) DEFAULT NULL,
-  `observacao` varchar(100) DEFAULT NULL
+  `observacao` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela oficina.cliente: ~0 rows (aproximadamente)
+DELETE FROM `cliente`;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` (`id`, `nome`, `cpf`, `telefone`, `email`, `endereco`, `observacao`) VALUES
+	(1, 'OICNAOsncoi', '12048689647', '31993799016', 'caosij@oascmio.com', 'Riuahsiu', 'iuasbiasub');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela oficina.item_orcamento
+CREATE TABLE IF NOT EXISTS `item_orcamento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) DEFAULT NULL,
+  `valor` double DEFAULT NULL,
+  `observacao` varchar(50) DEFAULT NULL,
+  `idPeca` int(11) DEFAULT NULL,
+  `idServico` int(11) DEFAULT NULL,
+  `idOrcamento` int(11) DEFAULT NULL,
+  `idCliente` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_itemorcamento_pecas` (`idPeca`),
+  KEY `FK_itemorcamento_servico` (`idServico`),
+  KEY `FK_itemorcamento_orcamento` (`idOrcamento`),
+  KEY `FK_itemorcamento_cliente` (`idCliente`),
+  CONSTRAINT `FK_itemorcamento_cliente` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`id`),
+  CONSTRAINT `FK_itemorcamento_orcamento` FOREIGN KEY (`idOrcamento`) REFERENCES `orcamento` (`CodOrc`),
+  CONSTRAINT `FK_itemorcamento_pecas` FOREIGN KEY (`idPeca`) REFERENCES `pecas` (`CodPec`),
+  CONSTRAINT `FK_itemorcamento_servico` FOREIGN KEY (`idServico`) REFERENCES `servico` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `cliente`
---
+-- Copiando dados para a tabela oficina.item_orcamento: ~0 rows (aproximadamente)
+DELETE FROM `item_orcamento`;
+/*!40000 ALTER TABLE `item_orcamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item_orcamento` ENABLE KEYS */;
 
-INSERT INTO `cliente` (`id`, `nome`, `cpf`, `telefone`, `email`, `endereco`, `observacao`) VALUES
-(1, 'OICNAOsncoi', '12048689647', '31993799016', 'caosij@oascmio.com', 'Riuahsiu', 'iuasbiasub');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `orcamento`
---
-
-CREATE TABLE `orcamento` (
-  `CodOrc` int(10) NOT NULL,
+-- Copiando estrutura para tabela oficina.orcamento
+CREATE TABLE IF NOT EXISTS `orcamento` (
+  `CodOrc` int(10) NOT NULL AUTO_INCREMENT,
   `ItemOrc` varchar(100) NOT NULL,
   `QtdeOrc` varchar(3) DEFAULT NULL,
-  `TotOrc` varchar(7) DEFAULT NULL
+  `TotOrc` varchar(7) DEFAULT NULL,
+  PRIMARY KEY (`CodOrc`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- Copiando dados para a tabela oficina.orcamento: ~0 rows (aproximadamente)
+DELETE FROM `orcamento`;
+/*!40000 ALTER TABLE `orcamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orcamento` ENABLE KEYS */;
 
---
--- Estrutura da tabela `pecas`
---
-
-CREATE TABLE `pecas` (
-  `CodPec` int(11) NOT NULL,
+-- Copiando estrutura para tabela oficina.pecas
+CREATE TABLE IF NOT EXISTS `pecas` (
+  `CodPec` int(11) NOT NULL AUTO_INCREMENT,
   `NomePec` varchar(100) DEFAULT NULL,
   `QtdePec` varchar(3) DEFAULT NULL,
-  `ValPec` varchar(8) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `ValPec` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`CodPec`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- Copiando dados para a tabela oficina.pecas: ~0 rows (aproximadamente)
+DELETE FROM `pecas`;
+/*!40000 ALTER TABLE `pecas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pecas` ENABLE KEYS */;
 
---
--- Estrutura da tabela `servico`
---
-
-CREATE TABLE `servico` (
-  `id` int(100) NOT NULL,
+-- Copiando estrutura para tabela oficina.servico
+CREATE TABLE IF NOT EXISTS `servico` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `nomeServico` varchar(100) DEFAULT NULL,
-  `valor` varchar(1000) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `valor` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `servico`
---
-
+-- Copiando dados para a tabela oficina.servico: ~0 rows (aproximadamente)
+DELETE FROM `servico`;
+/*!40000 ALTER TABLE `servico` DISABLE KEYS */;
 INSERT INTO `servico` (`id`, `nomeServico`, `valor`) VALUES
-(1, 'oi', '188,');
+	(1, 'oi', '188,');
+/*!40000 ALTER TABLE `servico` ENABLE KEYS */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `orcamento`
---
-ALTER TABLE `orcamento`
-  ADD PRIMARY KEY (`CodOrc`);
-
---
--- Indexes for table `pecas`
---
-ALTER TABLE `pecas`
-  ADD PRIMARY KEY (`CodPec`);
-
---
--- Indexes for table `servico`
---
-ALTER TABLE `servico`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `orcamento`
---
-ALTER TABLE `orcamento`
-  MODIFY `CodOrc` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pecas`
---
-ALTER TABLE `pecas`
-  MODIFY `CodPec` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `servico`
---
-ALTER TABLE `servico`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
